285ec5369f9dbd262eed2223416d6014bc0679ab
dhananjaythomble <dhananjaythomble@gmail.com>
2023-02-08 16:29:50 +0530

    add BuyPage component. display 6 random product names

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Container } from "react-bootstrap";
+import { useState } from "react";
+import BuyPage from "./components/BuyPage";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (item) => {
+    // check if the item is already present in cart
+    const isAlreadyAdded = cartItem.findIndex((arr) => {
+      return arr.id === item.id;
+    });
+
+    if (isAlreadyAdded !== -1) {
+      toast.error("Item is already present in cart");
+    } else {
+      setCartItem([...cartItem, item]);
+    }
+  };
+
+  const buyNow = () => {
+    setCartItem([]);
+  };
+
+  const removeItem = (item) => {
+    setCartItem(cartItem.filter((arrItem) => arrItem.id !== item.id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <BuyPage />
+    </Container>
   );
 }
 
---
+++ src/components/BuyPage.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { random, commerce } from "faker";
+import { Container, Row, Col } from "react-bootstrap";
+
+const apiKey = process.env.PEXELS_API;
+
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
+
+  const fetchPhotos = async () => {
+    const { data } = await Axios.get(url, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+
+    const { photos } = data;
+
+    const allProduct = photos.map((photo) => ({
+      smallImage: photo.src.medium,
+      tinyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
+
+    setProduct(allProduct);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+        {product.map((product) => (
+          <Col md={4} key={product.id}>
+            <h5>{product.productName}</h5>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default BuyPage;
